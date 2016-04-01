3caaca2ca58228ad3dff1ebff3e4979fa25b7c27
Jeremy Magland <jeremy.magland@gmail.com>
2016-04-01 10:46:59 -0400

    bug fix

--- src/mda/diskreadmda.cpp
+++ src/mda/diskreadmda.cpp
@@ -154,7 +154,7 @@ bool DiskReadMda::readChunk(Mda &X, long i, long size) const
         return true;
     }
 	if (!d->open_file_if_needed()) return false;
-	X.allocate(1,size);
+    X.allocate(size,1);
 	long jA=qMax(i,0L);
     long jB=qMin(i+size-1,d->total_size()-1);
 	long size_to_read=jB-jA+1;
@@ -256,7 +256,6 @@ bool DiskReadMda::readChunk(Mda &X, long i1, long i2, long i3, long size1, long 
 
 double DiskReadMda::value(long i) const
 {
-    qWarning() << "Don't use DiskReadMda::value dude";
 	if (d->m_use_memory_mda) return d->m_memory_mda.value(i);
     if ((i<0)||(i>=d->total_size())) return 0;
 	long chunk_index=i/DEFAULT_CHUNK_SIZE;
